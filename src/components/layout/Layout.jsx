97642513bb600c54ec07d830634a18acf6dedeb0
premchand.k <premchand.k@thingsalive.io>
2025-12-29 11:53:42 +0000

    google analytics changes

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
 import Footer from './Footer'; 
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import ScrollToTop from '../ScrollToTop';
+import { trackPageView } from '../../ga';
 import './Layout.css';
 
 export default function Layout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    trackPageView(location.pathname + location.search);
+  }, [location]);
+
   return (
     <div className="layout-container">
       <ScrollToTop />
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
 import { HelmetProvider } from 'react-helmet-async';
+import initializeGA from './ga';
 import './index.css'
 import 'rsuite/dist/rsuite.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import './firebase';
+
+initializeGA();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
